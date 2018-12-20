e86ddba9037ad85cebbe5eee2139f511773071f7
Paulinacm <pauly.cm@gmail.com>
2018-12-20 11:31:51 -0300

    agregando boostrap y archivos, a CSS y HTML

--- data.js
+++ data.js
@@ -1,8 +1,15 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+
+const dataPokemon = POKEMON.pokemon;
+console.log(dataPokemon);
+
+const nombrePokemones = (dataPokemon) =>{
+  console.log(dataPokemon)
+}
+nombrePokemones(dataPokemon);
