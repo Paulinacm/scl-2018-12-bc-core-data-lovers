c0051fc7d04b89797140745ba1bbd4a1fc493079
Paulinacm <pauly.cm@gmail.com>
2018-12-21 10:24:13 -0300

    eliminados elementos inneesarios en responsive CSS y HTML

--- src/main.js
+++ src/main.js
@@ -1,41 +1,4 @@
-$(document).ready(function(){
-
-
-
-
-	/*  Hamburger Menu & Icon  */
-	$('.hamburger').on('click', function(e){
-		
-		e.preventDefault();
-		$(this).toggleClass('opned');
-		$('header nav').toggleClass('active');
-		
-	});
-
-
-
-
-	/*  Advanced search form & Icon  */
-	$('#advanced_search_btn').on("click", function(e){
-		e.preventDefault();
-
-		var ads_box =$('.advanced_search');
-		
-		if(!ads_box.hasClass('advanced_displayed')){
-
-			$(this).addClass('active');
-			ads_box.stop().fadeIn(200).addClass('advanced_displayed');
-
-		}else{
-
-			$(this).removeClass('active');
-			ads_box.stop().fadeOut(200).removeClass('advanced_displayed');
-
-		}
-
-	});
-
-
-
-
-});
+/*
+for (i = 0; i < resultadoImagen.length; i++) {
+  document.getElementById("root").innerHTML += "<img src=" + resultadoImagen[i] + ">";
+}
