256a85ed156efeccb9145251925f9e5b11919d6e
Paulinacm <pauly.cm@gmail.com>
2019-01-02 13:04:47 -0300

    se eliminaron elementos del HTML y CSS, se aadieron carpetas para Test, trabajando en JS

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,4 @@
 require('../src/data.js');
-
-
 describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
@@ -10,3 +8,43 @@ describe('example', () => {
     expect(example()).toBe('example');
   });
 });
+
+
+/* el ejemplo del trabajo con Cipher
+describe('cipher', () => {
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+      assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () => {
+      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz', 33), 'hijklmnopqrstuvwxyzabcdefg');
+    });
+    it('debería retornar "8901234567" para "1234567890" con offest 33', () => {
+      assert.equal(cipher.encode('1234567890', 33), '8901234567');
+    });
+  });
+
+  describe('cipher.decode', () => {
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+      assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', 33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+      assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg', 33), 'abcdefghijklmnopqrstuvwxyz');
+    });
+    it('debería retornar "1234567890" para "8901234567" con offset 33', () => {
+      assert.equal(cipher.decode('8901234567', 33), '1234567890');
+    });
+  });
+});
+*/
